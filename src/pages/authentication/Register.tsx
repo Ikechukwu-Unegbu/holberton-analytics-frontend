376632ffe1f3048142ca74d15e2885ea47650aba
ikechukwu-unegbu <iunegbu94@yahoo.com>
2023-06-05 15:16:46 +0100

    implement basic auth pages

--- src/pages/authentication/Register.tsx
+++ src/pages/authentication/Register.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Auth.css'
+import Menu from '../components/Menu';
 
 interface RegisterProps {
   // Define any props you need for the component
@@ -17,36 +18,57 @@ const Register: React.FC<RegisterProps> = () => {
 
   return (
     <div>
-      <h2>Register</h2>
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+      <Menu/>
+      
+      <div className="auth"> 
+        <div className="auth-left">
+          <img className="img img-fluid" src="https://img.freepik.com/premium-vector/color-modern-illustration-data-analysis_145666-902.jpg?w=740" alt="" />
+        </div>
+        <div className="auth-right">
+          <div className=""> 
+            <h4 className='text text-secondary'>Register</h4>
+            <h6 className='text-secondary'>Holberton Analytics</h6>
+          </div>
+          <form>
+            <div className="form-group mt-3">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                className='form-control'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+            <div className="form-group mt-3">
+              <label htmlFor="password" className="form-label">Password:</label>
+              <input 
+                className='form-control'
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
 
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+           <div className='form-group mt-3'>
+            <label htmlFor="email" className="form-label">Email:</label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+           </div>
 
-        <button type="button" onClick={handleRegister}>
-          Register
-        </button>
-      </form>
+            <button type="button" className="btn btn-primary mt-2" onClick={handleRegister}>
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
